499a043ffbc092fc4981d54c6f6e3a90ca772386
Maurice Mickens <mmickens@squareup.com>
2026-02-25 17:15:12 -0500

    Initial Commit

---
+++ NearbyPaymentsAnimations/NearbyPaymentsAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearbyPaymentsAnimationsApp.swift
+//  NearbyPaymentsAnimations
+//
+//  Created by mmickens on 2/25/26.
+//
+
+import SwiftUI
+
+@main
+struct NearbyPaymentsAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
